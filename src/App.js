785766b02ee9c9468e852c9c818b53aa342687bf
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-15 23:22:54 -0400

    added back EmptySlot.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import { useState } from 'react'
 import MoleContainer from './components/MoleContainer';
-
+import EmptySlot from './components/EmptySlot';
 
 function App() {
   let [score, setScore] = useState(0)
 
   const createMoleHill = () => {
     let hills = []
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 3; i++) {
       hills.push(
         <MoleContainer 
         key={i} 
---
+++ src/components/EmptySlot.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import hillImg from '../img/molehill.png'
+
+function EmptySlot() {
+    return (
+        <div>
+            <img src={hillImg}/>
+        </div>
+    )
+}
+
+export default EmptySlot
