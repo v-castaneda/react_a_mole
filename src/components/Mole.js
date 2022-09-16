0b0e86259a6f9cb7276370908f31e6beea0c9d72
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-15 23:53:17 -0400

    added useEffect to Mole

--- src/components/Mole.js
+++ src/components/Mole.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import moleImg from '../img/mole.png'
 
-function Mole() {
+function Mole(props) {
+    useEffect(() => {
+        let randSec = Math.ceil(Math.random() * 3000)
+        let timer = setTimeout(() => {
+            props.setDisplayMole(false)
+        }, randSec)
+        return () => clearTimeout(timer)
+    })
+
     return (
         <div>
-            <img src={moleImg}/>
+            <img 
+            style={{'width': '30vw'}}
+            src={moleImg} 
+            onClick={props.handleClick}
+            />
         </div>
     )
 }
