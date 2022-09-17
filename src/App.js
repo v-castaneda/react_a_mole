d17f095b6dbd47835a60bbf7a3e4ea52bc563c00
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-16 23:37:21 -0400

    added MoleContainer functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import { useState } from 'react'
 import MoleContainer from './components/MoleContainer';
-import EmptySlot from './components/EmptySlot';
 
 function App() {
   let [score, setScore] = useState(0)
 
-  const createMoleHill = () => {
+  function createMoleHill() {
     let hills = []
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 6; i++) {
       hills.push(
         <MoleContainer 
         key={i} 
@@ -16,7 +15,6 @@ function App() {
         setScore={setScore} />
       )
     }
-
     return (
       <div>
         {hills}
--- src/components/MoleContainer.js
+++ src/components/MoleContainer.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
 import Mole from './Mole'
+import EmptySlot from './EmptySlot'
 
 function MoleContainer(props) {
+    let [theMole, setTheMole] = useState(false)
+
+    function handleClick() {
+        props.setScore(props.score + 1)
+        setTheMole(false)
+    }
+
+    let displayMole = theMole ? 
+        <Mole 
+            setScore={props.setScore} 
+            toggle={setTheMole} 
+            handleClick={handleClick} 
+        /> : 
+        <EmptySlot 
+            toggle={setTheMole} 
+        />
+
     return (
-        <div>
-            <Mole />
+        <div style={{'display': 'inline-block', 'width': '30vw'}}>
+            {displayMole}
         </div>
     )
 }
